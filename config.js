69865dfb0bc235eebafe619e8d0a0c4b60767c78
No.Error.No.Life.404 <56861806+akihitoarata@users.noreply.github.com>
2022-10-19 23:34:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Myuji';
+const giftUrl = '';
+const eventName = 'Happy Women`s Day';
+const titleCard = 'To You';
+const contentCard = 'Happy women`s day to you Myuji. What I`m most worried about is your health. You are always busy and sometimes forget to take care of yourself. You act like an angel and you`re an exquisite girl Myuji. You deserve to relax. I hope the best will always come to you. Hope you always have good health to enjoy the beauty of this life once again ~';
+
+const giftImage = 'Myu2.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
